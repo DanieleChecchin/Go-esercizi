b33f3af4cbca3c59d1205fd2cc2e25493ba8dcb7
Daniele Checchin <danielechecchin26@gmail.com>
2025-08-08 10:28:43 +0200

    hello world, sum

---
+++ sum.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func sum() {
+
+	// dichiaro le due variabili
+
+	numOne := 5
+	numTwo := 10
+
+	// sommo le due variabili
+	result := numOne + numTwo
+
+	// stampo il risultato
+	fmt.Println("La somma di", numOne, "e", numTwo, "è:", result)
+}
